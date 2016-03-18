f047eaa9335d8571f69c3aff5f343041d9ad18af
U-GMO\usr0101039 <usr0101039@JIND1606.GMO.LOCAL>
2016-03-18 20:53:13 +0900

    first commit

---
+++ 102-on-cygwin-go-bench-fio-seq-1G.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+cmd_fio="/cygdrive/c/Program\ Files/fio/fio"
+
+cur_dir="/cygdrive/c/cygwin/home/usr0101039/devel/Benchmark"
+conf_fio="$cur_dir/sequential.fio"
+
+
+cur_dir="C:\\\\cygwin\\\\home\\\\usr0101039\\\\devel\\\\Benchmark"
+##conf_fio="$cur_dir\\\\sequential.fio"
+##conf_fio="$cur_dir\\\\bench-sequential.fio"
+conf_fio="$cur_dir\\\\bench-sequential-1G.fio"
+
+
+
+pushd f:/fio/
+
+touch bench
+## dd if=/dev/urandom of=
+## head -c 128G /dev/urandom > bench
+head -c 1G /dev/urandom > bench
+
+eval $cmd_fio $conf_fio
+
+popd
+
+
---
+++ README.md
@@ -0,0 +1,6 @@
+# Win_Benchmark
+
+
+This is windows fio benchmark:
+
+reference: https://www.thomas-krenn.com/de/wiki/FIO_unter_Windows_nutzen
